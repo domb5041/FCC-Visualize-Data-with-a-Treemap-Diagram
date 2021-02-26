bd0e7a46d5dbd8061e34f969821e6fdbbfe149e8
dominicbutler <dominic.butler@exonar.com>
2021-02-26 09:11:21 +0000

    tooltip and legend

--- graph.js
+++ graph.js
@@ -8,7 +8,7 @@ d3.json(gameSalesFile, function (error, data) {
     console.log(data);
 
     const w = 1000;
-    const h = 800;
+    const h = 1000;
 
     const color = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -19,7 +19,17 @@ d3.json(gameSalesFile, function (error, data) {
         .attr('width', w)
         .attr('height', h);
 
-    const treemap = d3.treemap().size([w, h]).paddingInner(1);
+    const tooltip = d3
+        .select('#chart')
+        .append('div')
+        .attr('class', 'tooltip')
+        .attr('id', 'tooltip')
+        .style('opacity', 0);
+
+    const treemap = d3
+        .treemap()
+        .size([w, h - 200])
+        .paddingInner(1);
 
     const root = d3
         .hierarchy(data)
@@ -49,7 +59,25 @@ d3.json(gameSalesFile, function (error, data) {
         .attr('fill', d => color(d.data.category))
         .attr('data-name', d => d.data.name)
         .attr('data-category', d => d.data.category)
-        .attr('data-value', d => d.data.value);
+        .attr('data-value', d => d.data.value)
+        .on('mousemove', d => {
+            tooltip.style('opacity', 0.9);
+            tooltip
+                .html(
+                    'Name: ' +
+                        d.data.name +
+                        '<br>Category: ' +
+                        d.data.category +
+                        '<br>Value: ' +
+                        d.data.value
+                )
+                .attr('data-value', d.data.value)
+                .style('left', d3.event.pageX + 10 + 'px')
+                .style('top', d3.event.pageY - 28 + 'px');
+        })
+        .on('mouseout', () => {
+            tooltip.style('opacity', 0);
+        });
 
     cell.append('text')
         .attr('class', 'tile-text')
@@ -61,25 +89,33 @@ d3.json(gameSalesFile, function (error, data) {
         .attr('y', (d, i) => 13 + i * 10)
         .text(d => d);
 
-    // const legend = svg.append('g').attr('id', 'legend');
+    const legend = svg.append('g').attr('id', 'legend');
 
-    // legend
-    //     .selectAll('rect')
-    //     .data(cScale.domain())
-    //     .enter()
-    //     .append('rect')
-    //     .attr('x', (d, i) => p + i * 50)
-    //     .attr('y', 5)
-    //     .attr('width', 50)
-    //     .attr('height', 20)
-    //     .style('fill', d => cScale(d - 0.1));
+    var categories = root.leaves().map(function (nodes) {
+        return nodes.data.category;
+    });
+    categories = categories.filter(function (category, index, self) {
+        return self.indexOf(category) === index;
+    });
 
-    // legend
-    //     .selectAll('text')
-    //     .data(cScale.domain())
-    //     .enter()
-    //     .append('text')
-    //     .attr('x', (d, i) => p + 45 + i * 50)
-    //     .attr('y', 40)
-    //     .text(d => d);
+    legend
+        .selectAll('rect')
+        .data(color.domain())
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => 10 + i * 50)
+        .attr('y', h - 100)
+        .attr('width', 50)
+        .attr('height', 20)
+        .attr('class', 'legend-item')
+        .style('fill', d => color(d));
+
+    legend
+        .selectAll('text')
+        .data(color.domain())
+        .enter()
+        .append('text')
+        .attr('x', (d, i) => 20 + i * 50)
+        .attr('y', h - 105)
+        .text(d => d);
 });
